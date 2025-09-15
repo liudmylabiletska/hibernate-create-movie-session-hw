c93348c69cd478378bf5b920217a609170a95042
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-09-15 07:32:36 +0200

    create addea CinemaHall.java, CinemaHallDao.java, CinemaHallDaoImpl.java, CinemaHallService.java,CinemaHallServiceImpl.java, hibarnate.cfg.xml, Main.java, MovieSession.java, MovieSessionDao.java, MovieSessionDaoImpl.java, MovieSessionService.java, MovieSessionServiceImpl.java and pom.xml and modified CinemaHall.java, Main.java modidifed hibernate.cfg.xml, modified MovieServiceImpl.java

--- src/main/java/mate/academy/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -20,7 +20,7 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Movie get(Long id) {
         return movieDao.get(id).orElseThrow(
-                () -> new RuntimeException("Movie with id " + id + " not found"));
+                () -> new NotFoundException("Movie with id " + id + " not found"));
     }
 
     @Override
